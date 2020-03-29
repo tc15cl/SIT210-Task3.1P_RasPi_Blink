22e3dcbd3f27d7c595cdf2f3ce3247a221cf4548
Tim Cook <tc15cl@gmail.com>
2020-03-29 16:54:51 +1100

    First

---
+++ blinker.py
@@ -0,0 +1,27 @@
+#
+# Project: RasPi Blink (SIT210-05.1P)
+# Description: Blinking LED on Raspberry Pi
+# Author: tjcook (214014891)
+# Date: 29/03/20
+
+#Import raspberry pi GPIO & time libraries
+import RPi.GPIO as GPIO
+from time import sleep
+
+#Set pinout mode (BCM or physical)
+GPIO.setmode(GPIO.BCM)
+
+#Set pin as output
+GPIO.setup(20, GPIO.OUT)
+
+#Blink LED and sleep every 250ms, stop and clean when CTRL+C pressed
+try:
+    while 1:
+        GPIO.output(20, GPIO.HIGH)
+        sleep(0.25)
+        GPIO.output(20, GPIO.LOW)
+        sleep(0.25)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+
+
